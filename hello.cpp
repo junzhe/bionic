a82eab0e9c51b683f5ede1e1b909f216618aa18d
Junzhe Zhang <junzhe@umich.edu>
2014-01-28 22:29:59 -0500

    test

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <machine/limits.h>
 #include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 using namespace std;
 
 int main(){
-	alarm(10);
 	cout<<"Hello World!"<<endl;
+	int     shmid;
+        
+        if((shmid = shmget( keyval, segsize, IPC_CREAT | 0660 )) == -1)
+        {
+                return(-1);
+        }
+
 	return 1;
 }
